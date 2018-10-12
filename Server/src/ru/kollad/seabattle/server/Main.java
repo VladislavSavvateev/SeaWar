3cb6a67e72ca9200390a8571c32d5e8d5a9fa039
NikolayNIK <nikitin.nikolay@bk.ru>
2018-10-12 18:39:32 +0800

    Server (probably even working)

--- Server/src/ru/kollad/seabattle/server/Main.java
+++ Server/src/ru/kollad/seabattle/server/Main.java
@@ -1,8 +1,54 @@
 package ru.kollad.seabattle.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		byte[] buffer = new byte[2];
+		Map<Integer, Room> rooms = new HashMap<>();
+		Random random = new Random(System.currentTimeMillis());
+		ServerSocket serverSocket = new ServerSocket(1337, 256);
+		while (!Thread.interrupted()) {
+			try {
+				Socket socket = serverSocket.accept();
+				switch (socket.getInputStream().read()) {
+					case 34:
+						random.nextBytes(buffer);
+						rooms.put(ByteBuffer.wrap(buffer).getInt(), new Room(socket));
+						socket.getOutputStream().write(buffer);
+						break;
+					case 69:
+						int i = socket.getInputStream().read(buffer);
+						if (i != 2) {
+							socket.getOutputStream().write(47);
+							socket.close();
+							break;
+						}
 
-    }
+						Room room = rooms.remove(ByteBuffer.wrap(buffer).getInt());
+						if (room == null) {
+							socket.getOutputStream().write(47);
+							socket.close();
+							break;
+						}
+
+						room.setOpponent(socket);
+						break;
+				}
+			} catch (SocketTimeoutException ignored) {
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		serverSocket.close();
+	}
 }
--- Server/src/ru/kollad/seabattle/server/Room.java
+++ Server/src/ru/kollad/seabattle/server/Room.java
@@ -1,4 +1,93 @@
 package ru.kollad.seabattle.server;
 
-public class Room {
+import java.io.IOException;
+import java.net.Socket;
+
+public class Room extends Thread {
+
+	private Socket socket1, socket2;
+
+	Room(Socket socket) {
+		this.socket1 = socket;
+	}
+
+	void setOpponent(Socket socket) {
+		this.socket2 = socket;
+		this.start();
+	}
+
+	@Override
+	public void run() {
+		try {
+			byte[] field1 = receiveField(socket1, 34);
+			byte[] field2 = receiveField(socket2, 69);
+			boolean whatever = false;
+			while (!Thread.interrupted()) {
+				if (whatever) turn(socket1, socket2, field2);
+				else turn(socket2, socket1, field1);
+				whatever = !whatever;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			socket1.close();
+			socket2.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private byte[] receiveField(Socket socket, int response) throws IOException {
+		byte[] bytes = new byte[8];
+		if (socket.getInputStream().read(bytes) != bytes.length)
+			throw new IOException();
+
+		socket.getOutputStream().write(response);
+		socket.getOutputStream().flush();
+
+		return bytes;
+	}
+
+	private void turn(Socket sender, Socket receiver, byte[] field) throws Exception {
+		int x = sender.getInputStream().read();
+		int y = sender.getInputStream().read();
+		byte row = field[y];
+		byte target = pow(x);
+
+		boolean done = true;
+		if ((row & target) == target) { // Hope for the best
+			field[y] = (byte) (row & ~target);
+			for (byte b : field) {
+				if (b != 0) {
+					done = false;
+					break;
+				}
+			}
+
+			sender.getOutputStream().write(done ? 34 : 69);
+		} else {
+			sender.getOutputStream().write(47);
+		}
+
+		sender.getOutputStream().flush();
+
+		if (done) {
+			receiver.getOutputStream().write(69);
+			receiver.getOutputStream().flush();
+			throw new Exception("Game finished!");
+		} else {
+			receiver.getOutputStream().write(x);
+			receiver.getOutputStream().write(y);
+			receiver.getOutputStream().flush();
+		}
+	}
+
+	private byte pow(int power) {
+		byte result = 1;
+		for (int i = 0; i < power; i++)
+			result *= 2;
+		return result;
+	}
 }
