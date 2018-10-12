5d5ab09db89358a782d1cfe5298b8b3aa57f7c95
Vladislav Savvateev <savvateevvlad@mail.ru>
2018-10-12 17:20:33 +0800

    Initial commit with client

---
+++ SeaWar.Client/SeaWar.Client/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+
+namespace SeaWar.Client {
+	class Program {
+		public static void Main(string[] args) {
+			Console.Write("SeaWar.Client v1.0 by VladislavSavvateev\n\nEnter host: ");
+			
+		}
+	}
+}
